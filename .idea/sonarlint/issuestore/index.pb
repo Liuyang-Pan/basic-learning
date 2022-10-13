aa8d81b51b33790dade5e2429f81b253e4420336
Pan Liuyang <1679814115@qq.com>
2022-10-14 00:47:28 +0800

    BiasLock偏向锁

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,9 @@ Lbasic/src/main/java/org/example/access/packageone/DefaultMemberVariable.java,f
 basic/pom.xml,d\8\d8300307182777cf15b520a20c802ac9f3279ba3
 u
 Ebasic/src/main/java/org/example/access/packageone/MemberVariable.java,e\f\ef245ae9c7f93c0b4e1719e538a772e96ec292c9
+l
+<concurrent/src/main/java/org/example/thread/ThreadUsage.java,1\5\153cae327f334daaa021c0e2d7139ef1600608f2
+l
+<concurrent/src/main/java/org/example/thread/ThreadState.java,a\f\afe43c2aa9f5d9e56bb2744949a3c0da517566f2
+s
+Cconcurrent/src/main/java/org/example/thread/ThreadCommonMethod.java,f\d\fdddb18097b761623eaa3c8c7c3feaec85aaef6b
--- concurrent/src/main/java/org/example/shared/SynchronizedOptimization.java
+++ concurrent/src/main/java/org/example/shared/SynchronizedOptimization.java
@@ -1,5 +1,10 @@
 package org.example.shared;
 
+import lombok.extern.slf4j.Slf4j;
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * purpose:synchronized优化原理进阶
  * <p>
@@ -16,7 +21,12 @@ package org.example.shared;
  * <p>
  * 4.偏向锁
  * 轻量级锁在没有竞争时(就自己这个线程)，每次重入仍然需要执行CAS操作。
- * Java 6中引入了偏向锁来做进一步优化:只有第一次使用CAS将线程ID设置到对象的Mark Word头，之后发现这个线程D是自己的就表示没有竞争，不用重新CAS。以后只要不发生竞争，这个对象就归该线程所有
+ * Java 6中引入了偏向锁来做进一步优化:只有第一次使用CAS将线程ID设置到对象的 Mark Word头，之后发现这个线程ID是自己的就表示没有竞争，不用重新CAS。以后只要不发生竞争，这个对象就归该线程所有。
+ * 对象创建时:
+ * 如果开启了偏向锁（默认开启)，那么对象创建后，mark word值为0x05即最后3位为101，这时它的thread、epoch、age都为О
+ * 偏向锁是默认是延迟的，不会在程序启动时立即生效，如果想避免延迟，可以加VM参数-XX:BiasedLockingStartupDelay=0来禁用延迟
+ * 如果没有开启偏向锁，那么对象创建后，mark word值为0x01即最后3位为001，这时它的hashcode、age都为0，第一次用到hashcode时才会赋值
+ * VM参数-XX:-UseBiasedLocking 禁用偏向锁 -XX:+UseBiasedLocking 开启偏向锁
  *
  * @author Pan Liuyang
  * 2022/10/13 17:22
@@ -24,6 +34,10 @@ package org.example.shared;
 public class SynchronizedOptimization {
     static final Object lock = new Object();
 
+    /**
+     * 同一个线程方法1调用方法2重入锁（重入锁会让锁对象的Mark Word对象头再次做CAS操作，会有性能的损耗）
+     * 偏向锁对重入锁进行了优化，将ThreadId记录到对象头中
+     */
     public static void method1() {
         synchronized (lock) {
             //同步块A
@@ -35,5 +49,38 @@ public class SynchronizedOptimization {
         synchronized (lock) {
             //同步块B
         }
+    }
+}
+
+class BiasObject {
+
+}
+
+/**
+ * 偏向锁
+ */
+@Slf4j
+class ThreadBiasLock {
+    public static void main(String[] args) throws InterruptedException {
+        BiasObject biasObject = new BiasObject();
+        //调用hashCode会撤销偏向锁，因为对象头存不下偏向锁的线程ID了。调用wait和notify也会撤销偏向锁，因为wait和notify只有重量级锁才有
+//        biasObject.hashCode();
+        String biasObjectClassLayout = ClassLayout.parseInstance(biasObject).toPrintable();
+        log.info(biasObjectClassLayout);
+
+        synchronized (biasObject) {
+            log.info(biasObjectClassLayout);
+        }
+
+        synchronized (biasObject) {
+            log.info(biasObjectClassLayout);
+        }
+
+        log.info(biasObjectClassLayout);
+
+        TimeUnit.SECONDS.sleep(4);
+        //偏向锁延迟
+        String biasObjectClassLayoutNew = ClassLayout.parseInstance(new BiasObject()).toPrintable();
+        log.info(biasObjectClassLayoutNew);
     }
 }
