101e44e62a0a5035e6ab2ea8a5f622e43968e9f2
Pan Liuyang <1679814115@qq.com>
2022-10-26 17:44:00 +0800

    Basic

---
+++ basic-17/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        for (short s = 0; s < 5; s++) {
+            System.out.println("Hello world!");
+        }
+        int i = 1500;
+        byte b = (byte) i;
+        System.out.println(b);
+
+        System.out.println(5 + "a");
+    }
+}
