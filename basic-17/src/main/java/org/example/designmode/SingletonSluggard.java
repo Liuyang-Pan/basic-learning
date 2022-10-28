7ebebd751058d9ae5a091d01582b57e140d827b8
Pan Liuyang <1679814115@qq.com>
2022-10-28 13:10:29 +0800

    Singleton

--- basic-17/src/main/java/org/example/designmode/SingletonSluggard.java
+++ basic-17/src/main/java/org/example/designmode/SingletonSluggard.java
@@ -2,11 +2,16 @@ package org.example.designmode;
 
 /**
  * 懒汉单例模式：在调用getInstance方法时才在堆内存中创建一个实例
- * 非线程安全的
+ * 非线程安全的，需要加synchronized关键字
  */
 public class SingletonSluggard {
     private static SingletonSluggard singletonSluggard;
 
+    /**
+     * 若不加synchronized则是非线程安全的
+     *
+     * @return SingletonSluggard实例
+     */
     public static synchronized SingletonSluggard getInstance() {
         if (null == singletonSluggard) {
             singletonSluggard = new SingletonSluggard();
